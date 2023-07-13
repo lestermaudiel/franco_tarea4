26811ac6b2c07b233ae71f85a3ef644abfe9217b
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-12 22:20:47 -0600

    se crea el evento DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.getElementById('formulario'); 
+    const contenedorInputs = document.getElementById('inputText'); 
+    const agregarInput = document.getElementById('agregar'); 
+    const eliminarInput = document.getElementById('eliminar');
+});
